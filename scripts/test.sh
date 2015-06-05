46b6efbec5c8b0daebb1127823e03b25f2b88bab
Tony Ganch <tonyganch@gmail.com>
2015-06-05 23:17:33 +0200

    Remove gulp in favour of bash scripts

---
+++ scripts/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+EXIT_CODE=0
+
+function test {
+    "$@"
+    if [ $? -ne 0 ]; then
+        EXIT_CODE=1
+    fi
+}
+
+# Run tests
+printf "\n\
+---------------\n\
+ Running Mocha\n\
+---------------\n\n"
+test ./node_modules/.bin/mocha
+
+if [ $EXIT_CODE -ne 0 ]; then
+printf "\n\
+----------------------------------------------------\n\
+ Please, fix errors shown above and run tests again\n\
+----------------------------------------------------\n"
+else
+printf "\n\
+------------------------\n\
+ Everything looks fine!\n\
+------------------------\n"
+fi
+
+exit $EXIT_CODE
