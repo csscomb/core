dba23f700d78dadc2e63ad48475525cac34caad1
Tony Ganch <tonyganch@gmail.com>
2015-06-20 20:51:27 +0300

    Core: Rewrite lint & process sections + add tests

--- test/core.js
+++ test/core.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var gonzales = require('gonzales-pe');
+var fs = require('fs');
 var minimatch = require('minimatch');
 var Comb = require('../lib/core');
 
@@ -45,10 +45,161 @@ describe('Core methods', function() {
     });
   });
 
-  describe('lintTree', function() {
+  describe('lintDirectory', function() {
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+
     it('should return promise', function() {
-      var ast = require('./helpers/ast-1');
+      var dir = __dirname;
+      assert(comb.lintDirectory(dir).then, 'function');
+    });
+
+    it('should lint all files in the directory', function(done) {
+      var dir = __dirname + '/helpers';
+      comb.use(plugin).configure({option1: true})
+          .lintDirectory(dir).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+
+    it('should lint all files recursively', function(done) {
+      var dir = __dirname;
+      comb.use(plugin).configure({option1: true})
+          .lintDirectory(dir).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+  });
+
+  describe('lintFile', function() {
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+    var cssFile = __dirname + '/helpers/file.css';
+    var jsFile = __dirname + '/helpers/ast_css.js';
+
+    it('should return promise', function() {
+      assert(comb.lintFile(cssFile).then, 'function');
+    });
+
+    it('should return 1 error', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintFile(cssFile).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+
+    it('should skip unacceptable file', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintFile(jsFile).catch(function() {
+            done();
+          });
+    });
+  });
+
+  describe('lintPath', function() {
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+    var file = __dirname + '/helpers/file.css';
+    var dir = __dirname;
+
+    it('should return promise', function() {
+      assert(comb.lintPath(file).then, 'function');
+    });
+
+    it('should lint a file', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintPath(file).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+
+    it('should lint a directory', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintPath(dir).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+  });
+
+  describe('lintString', function() {
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+    var string = fs.readFileSync(__dirname + '/helpers/file.css', 'utf-8');
+
+    it('should return promise', function() {
+      assert(comb.lintString(string).then, 'function');
+    });
+
+    it('should return 1 error', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintString(string).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+  });
+
+  describe('lintTree', function() {
+    var ast = require('./helpers/ast_css');
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+
+    it('should return promise', function() {
       assert(typeof comb.lintTree(ast).then, 'function');
+    });
+
+    it('should return 1 error', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintTree(ast).then(function(errors) {
+            assert.equal(errors.length, 1);
+            done();
+          });
+    });
+
+    it('should add filename to errors', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .lintTree(ast, 'panda').then(function(errors) {
+            assert.equal(errors[0].filename, 'panda');
+            done();
+          });
+    });
+  });
+
+  describe('processString', function() {
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+    var string = fs.readFileSync(__dirname + '/helpers/file.css', 'utf-8');
+
+    it('should return promise', function() {
+      assert(typeof comb.processString(string).then, 'function');
+    });
+
+    it('should return modified string', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .processString(string).then(function(modifiedString) {
+            var numberOfSpaces = modifiedString.split(' ').length - 1;
+            // There should be 2 spaces left inside comment.
+            assert.equal(numberOfSpaces, 2);
+            done();
+          });
+    });
+  });
+
+  describe('processTree', function() {
+    var ast = require('./helpers/ast_css');
+    var plugin = require('./helpers/plugin_css_lint_1_error');
+
+    it('should return promise', function() {
+      assert(typeof comb.processTree(ast).then, 'function');
+    });
+
+    it('should return modified ast', function(done) {
+      comb.use(plugin).configure({option1: true})
+          .processTree(ast).then(function(ast) {
+            var numberOfSpaces = ast.toString().split(' ').length - 1;
+            // There should be 2 spaces left inside comment.
+            assert.equal(numberOfSpaces, 2);
+            done();
+          });
     });
   });
 });
