d6cf98c73e9be697afe990f7891091e995c34d18
Tony Ganch <tonyganch@gmail.com>
2015-06-20 14:37:42 +0300

    Tests: Add tests for Comb#configure

--- test/core.js
+++ test/core.js
@@ -1,3 +1,46 @@
+var assert = require('assert');
+var minimatch = require('minimatch');
+var Comb = require('../lib/core');
+
+var comb;
+
 describe('Core methods', function() {
-    it('', function() {});
+  beforeEach(function() {
+    comb = new Comb();
+  });
+
+  describe('configure', function() {
+    it('should throw with no config', function() {
+      assert.throws(function() {
+        comb.configure();
+      });
+    });
+
+    it('should throw if config is not an object', function() {
+      assert.throws(function() {
+        comb.configure('nani');
+      });
+    });
+
+    it('should copy simple fields from config', function() {
+      var config = {lint: false, verbose: true};
+      comb.configure(config);
+      assert.equal(comb.lint, config.lint);
+      assert.equal(comb.verbose, config.verbose);
+    });
+
+    it('should extend `exclude` patterns', function() {
+      var pattern = 'panda/**/*.ext';
+      var minimatchedPattern = new minimatch.Minimatch(pattern);
+      var config = {exclude: [pattern]};
+      comb.configure(config);
+      assert.deepEqual(comb.exclude, [minimatchedPattern]);
+    });
+
+    it('should not configure plugins that have not been added', function() {
+      var config = {animal: 'panda'};
+      comb.configure(config);
+      assert.deepEqual(comb.config, {});
+    });
+  });
 });
