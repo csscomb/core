4b3f7247d6c9d1fb148cabd8571fbe57ed2d508a
Tony Ganch <tonyganch@gmail.com>
2015-06-20 12:42:31 +0300

    Tests: Add test for message formatter

--- test/format.js
+++ test/format.js
@@ -1,3 +1,22 @@
+var assert = require('assert');
+var format = require('../lib/format');
+
 describe('Message formatter', function() {
-    it('', function() {});
+    it('should replace line breaks with a single space', function() {
+        var string = 'a\nb\nc\n';
+        var expected = 'a b c ';
+        assert.equal(expected, format(string));
+    });
+
+    it('should replace line breaks and multiple spaces with a single space', function() {
+        var string = 'a\n    b\n c\n';
+        var expected = 'a b c ';
+        assert.equal(expected, format(string));
+    });
+
+    it('should save spaces before line breaks', function() {
+        var string = 'a \nb\nc  \n';
+        var expected = 'a  b c   ';
+        assert.equal(expected, format(string));
+    });
 });
