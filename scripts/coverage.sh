189150cc80a4e7c3b320053d7b04183971c0ce72
Tony Ganch <tonyganch@gmail.com>
2015-06-20 22:06:12 +0300

    Tools: Fix coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,1 +1,1 @@
-./node_modules/.bin/unit-coverage run -a lib -s 'lib/**' -a test -t 'test/**' -r html -o coverage.html -- lib test
+./node_modules/.bin/unit-coverage run -a lib -s 'lib/*.js' -a test -t 'test/*.js' -r html -o coverage.html -- lib test
